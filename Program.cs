56916c76e4acaf6ea2fa0a3101b2527e5e39561a
Karl Eriksson <iuff12345@gmail.com>
2020-11-10 09:24:17 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till TodoList!");
+            Console.WriteLine("Kommandon: quit, load 'filename', save, save 'filename'");
+
+            bool check = false;
+            string command;
+            string[] commandWord;
+
+            do
+            {
+                Console.Write("> ");
+                command = Console.ReadLine();
+                commandWord = command.Split(' ');
+
+                switch (commandWord[0]){
+                    case "quit":
+                        check = true;
+                        Console.WriteLine("Goodbye!");
+                        break;
+                    case "load":
+                        
+                        break;
+                }
+
+            } while (!check);
+            
+
+        }
+    }
+}
